75880148ff74075c87914d77f18e60df40c06246
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-10-22 00:21:56 -0300

    forest tilesets

---
+++ levels/tilesets/forest/player_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version = "1.0" encoding = "UTF-8">
+<tileset version = "1.5" tiledversion = "1.10.2" name = "player_tiles" tilewidth = "64" tileheight = "64" tilecount = "3" columns = "3">
+    <image source = "../../../graphics/character/setup_tile.png" width = "192" height = "64"/>
+</tileset>
---
+++ levels/tilesets/forest/terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version = "1.0" encoding = "UTF-8">
+<tileset version = "1.5" tiledversion = "1.10.2" name = "terrain_tiles" tilewidth = "64" tileheight = "64" tilecount = "16" columns = "4">
+    <image source = "../../../graphics/forest/terrain/terrain.png" width = "256" height = "256"/>
+</tileset>
