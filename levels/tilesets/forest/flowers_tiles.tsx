96b823f0df1240678e210abdf448d38028b353c3
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-10-26 15:39:55 -0300

    New setup Level

--- levels/tilesets/forest/flowers_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version = "1.0" encoding = "UTF-8">
-<tileset version = "1.5" tiledversion = "1.10.2" name = "flowers_tiles" tilewidth = "64" tileheight = "64" tilecount = "4" columns = "4">
-    <image source = "../../../graphics/forest/decorations/flowers/flowers.png" width = "256" height = "64"/>
-</tileset>
