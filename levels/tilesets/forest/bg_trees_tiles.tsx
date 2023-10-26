30d42b4568278ae82e600b8faa70ecfef4b12ab3
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-10-26 16:56:19 -0300

    All level setup

---
+++ levels/tilesets/forest/bg_trees_tiles.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="bg_trees_tiles" tilewidth="132" tileheight="238" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="110" height="191" source="../../../graphics/forest/terrain/bg_trees_1/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="120" height="192" source="../../../graphics/forest/terrain/bg_trees_2/bg_tree_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="132" height="238" source="../../../graphics/forest/terrain/bg_trees_3/bg_tree_3.png"/>
+ </tile>
+</tileset>
