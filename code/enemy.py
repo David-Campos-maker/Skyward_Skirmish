6331015352216da9e1f480c1cddc1d88d3c9a6fb
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-10-28 21:49:10 -0300

    resizing enemies

--- code/enemy.py
+++ code/enemy.py
@@ -7,10 +7,11 @@ class Enemy(AnimatedTile):
         super().__init__(size , x , y , path = '../graphics/enemy/run')
         
         self.speed = randint(3 , 4)
-        
+        self.rect.y -= 38
+
     def move(self):
-        self.rect.x += self.speed 
-        
+        self.rect.x += self.speed
+
     def reverse_image(self):
         if self.speed > 0:
             self.image = pygame.transform.flip(self.image , True , False)
@@ -21,5 +22,6 @@ class Enemy(AnimatedTile):
     def update(self , shift):
         self.rect.x += shift
         self.animate()
+        self.image = pygame.transform.scale(self.image, (round(self.image.get_width() * 1.5), round(self.image.get_height() * 1.5)))
         self.move()
         self.reverse_image()
