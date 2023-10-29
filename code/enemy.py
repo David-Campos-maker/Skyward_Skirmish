16b8a602da311703c4f944f24b3c658e7e67f9d9
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-10-28 21:30:50 -0300

    reducing enemies speed

--- code/enemy.py
+++ code/enemy.py
@@ -6,7 +6,7 @@ class Enemy(AnimatedTile):
     def __init__(self , size , x , y):
         super().__init__(size , x , y , path = '../graphics/enemy/run')
         
-        self.speed = randint(3 , 5)
+        self.speed = randint(3 , 4)
         
     def move(self):
         self.rect.x += self.speed 
