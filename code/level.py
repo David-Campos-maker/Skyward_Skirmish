6f44e9b9404e834f4931a2933d549ad43bc6f281
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-11-23 17:09:55 -0300

    Level comments

--- code/level.py
+++ code/level.py
@@ -1,35 +1,3 @@
-# from settings import screen_width , screen_height
-
-# class Level:
-#     def __init__(self , current_level , surface , create_overworld):
-        
-#         #Level Setup
-#         self.display_surface = surface
-#         self.current_level = current_level
-#         level_data = levels[current_level]
-#         level_content = level_data['content']
-#         self.new_max_level = level_data['unlock']
-#         self.create_overworld = create_overworld
-        
-#         #Level Display
-#         self.font = pygame.font.Font(None , 40)
-#         self.text_surf = self.font.render(level_content , True , 'White')
-#         self.text_rect = self.text_surf.get_rect(center = (screen_width / 2 , screen_height / 2))
-
-#     def input(self):
-#         keys = pygame.key.get_pressed()
-        
-#         if keys[pygame.K_RETURN]:
-#             self.create_overworld(self.current_level , self.new_max_level)
-            
-#         if keys[pygame.K_ESCAPE]:
-#             self.create_overworld(self.current_level , 0)
-
-#     def run(self):
-#         self.input()
-#         self.display_surface.blit(self.text_surf , self.text_rect)
-
-
 import pygame
 from support import import_csv_layout , import_cut_graphic
 from settings import tile_size , screen_width , screen_height
