e4cc48658961af97e6ffe5a2a3a27685bf181959
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-11-25 22:55:38 -0300

    Player on center

--- code/level.py
+++ code/level.py
@@ -198,11 +198,11 @@ class Level:
         player_x = player.rect.centerx   
         direction_x = player.direction.x    
         
-        if player_x < screen_width / 3 and direction_x < 0:
+        if player_x < screen_width / 2 and direction_x < 0:
             self.world_shift = 6
             player.speed = 0 
             
-        elif player_x > screen_width - (screen_width / 3) and direction_x > 0:
+        elif player_x > screen_width - (screen_width / 2) and direction_x > 0:
             self.world_shift = -6
             player.speed = 0  
             
