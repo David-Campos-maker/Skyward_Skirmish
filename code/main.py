0db4a6fb0faf2660cd28958cc984abdd64de3264
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-11-27 21:42:19 -0300

    RAM Management

--- code/main.py
+++ code/main.py
@@ -6,46 +6,48 @@ from ui import UI
 
 class Game:
     def __init__(self):
-        
-        # Game Attributes
-        self.max_level = 0
-        self.max_health = 100
-        self.cur_health = 100
-        
-        # Overworld Creation
-        self.overworld = Overworld(0 , self.max_level , screen , self.create_level)
-        self.status = 'overworld'
-        
-        # User Interface
-        self.ui = UI(screen)
-        
+            
+            # Game Attributes
+            self.max_level = 0
+            self.max_health = 100
+            self.cur_health = 100
+            
+            # Overworld Creation
+            self.create_overworld(0 , self.max_level)
+            self.status = 'overworld'
+            
+            # User Interface
+            self.create_ui()
+            
     def create_level(self , current_level):
-        if hasattr(self, 'level'):
-            del self.level
+        if hasattr(self, 'level'): del self.level
             
         self.level = Level(current_level , screen , self.create_overworld , self.change_health)
         self.status = 'level'
         
     def create_overworld(self , current_level , new_max_level):
-        if new_max_level > self.max_level:
-            self.max_level = new_max_level
+        if hasattr(self, 'overworld'): del self.overworld
+        if new_max_level > self.max_level: self.max_level = new_max_level
             
         self.overworld = Overworld(current_level , self.max_level , screen , self.create_level)
         self.status = 'overworld'
-       
+    
+    def create_ui(self):
+        if hasattr(self, 'ui'): del self.ui
+        self.ui = UI(screen)
+        
     def change_health(self , amount):
         self.cur_health += amount
-      
+    
     def check_game_over(self):
         if self.cur_health <= 0:
             self.cur_health = 100
             self.max_level = 0
-            self.overworld = Overworld(0 , self.max_level , screen , self.create_level)
+            self.create_overworld(0 , self.max_level)
             self.status = 'overworld'
         
     def run(self):
-        if self.status == 'overworld':
-            self.overworld.run()
+        if self.status == 'overworld': self.overworld.run()
         else:
             self.level.run()
             self.ui.show_health(self.cur_health , self.max_health)
